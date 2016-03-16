d6ecaf4b889a25a1a5efd76a643e4c1d555fd922
asp1111 <403347810@qq.com>
2016-03-16 10:13:20 +0800

    fist

---
+++ SpringMVC/src/main/java/com/mqs/springMVCDemo/web/controller.java
@@ -0,0 +1,14 @@
+package com.mqs.springMVCDemo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("test/")
+public class controller {
+	
+	@RequestMapping("goIndex.do")
+	public String goIndex() {
+		return "/index.jsp";
+	}
+}
