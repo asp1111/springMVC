d1470db8085cdbcb54672d8dc66a9d60720fbed6
asp1111 <403347810@qq.com>
2016-03-16 17:21:33 +0800

    u

--- SpringMVC/src/main/java/com/mqs/springMVCDemo/service/impl/UserServiceImpl.java
+++ SpringMVC/src/main/java/com/mqs/springMVCDemo/service/impl/UserServiceImpl.java
@@ -2,15 +2,17 @@ package com.mqs.springMVCDemo.service.impl;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.mqs.springMVCDemo.dao.IUserDao;
 import com.mqs.springMVCDemo.entity.User;
 import com.mqs.springMVCDemo.service.IUserService;
 
 
-@Service("userService")
+@Service
 public class UserServiceImpl implements IUserService {
-	@Resource
+	@Autowired
 	private IUserDao userDao;
 	@Override
 	public User getUserById(int userId) {
--- SpringMVC/src/main/java/com/mqs/springMVCDemo/web/controller.java
+++ SpringMVC/src/main/java/com/mqs/springMVCDemo/web/controller.java
@@ -1,14 +1,25 @@
 package com.mqs.springMVCDemo.web;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mqs.springMVCDemo.entity.User;
+import com.mqs.springMVCDemo.service.IUserService;
+
 @Controller
-@RequestMapping("test/")
+@RequestMapping("/test/")
 public class controller {
 	
+	@Resource
+	private IUserService iUserService;
+	
 	@RequestMapping("goIndex.do")
-	public String goIndex() {
+	public String goIndex(Model model) {
+		User user = this.iUserService.getUserById(1);
+		model.addAttribute("user", user);
 		return "index.jsp";
 	}
 }
